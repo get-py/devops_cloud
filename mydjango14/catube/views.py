581af0fb7318a47511152fb6ed12e3c99cfaacac
Yerin Park <1472moment@gmail.com>
2021-12-02 15:14:47 +0900

    비디오 재생기능 추가

--- mydjango14/catube/views.py
+++ mydjango14/catube/views.py
@@ -1,6 +1,13 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from catube.models import Video
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "catube/index.html")
+    qs = Video.objects.all()
+    return render(request, "catube/index.html", {"video_list": qs}, )
+
+
+def video_detail(request: HttpRequest, pk: int) -> HttpResponse:
+    video = Video.objects.get(pk=pk)
+    return render(request, "catube/video_detail.html", {'video': video}, )
