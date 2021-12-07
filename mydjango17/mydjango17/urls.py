609c2cacbb7e999ce0edfaa71ee12268953c8f66
Yerin Park <1472moment@gmail.com>
2021-12-07 14:03:51 +0900

    root 뷰 구현

--- mydjango17/mydjango17/urls.py
+++ mydjango17/mydjango17/urls.py
@@ -1,16 +1,23 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
+
+
+def root(request):
+    return redirect("diary:post_list")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('diary/', include('diary.urls'))
+    path('diary/', include('diary.urls')),
+    path('', root, name='root')
 
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
-                      documents_root = settings.MEDIA_ROOT)
+                      document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
