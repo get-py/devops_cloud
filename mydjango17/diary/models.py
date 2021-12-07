fc1121ef5f06a5c1537c934da0733a8c7f292a98
Yerin Park <1472moment@gmail.com>
2021-12-07 11:02:58 +0900

    diary 모델 생성 및 migration

--- mydjango17/diary/models.py
+++ mydjango17/diary/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True  # 추상 클래스로서, DB 테이블이 생기지 않습니다.
+
+
+class Post(TimestampedModel):
+    title = models.CharField(max_length=200, db_index=True)
+    author_name = models.CharField(max_length=20)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='diary/post/%Y/%m/%d')
+    tag_set = models.ManyToManyField('Tag', blank=True)  # 장고가 알아서 찾아줌
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = "포스팅"
+        verbose_name_plural = "포스팅 목록"
+
+
+class Comment(TimestampedModel):
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name = "댓글"
+        verbose_name_plural = "댓글 목록"
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "태그"
+        verbose_name_plural = "태그 목록"
