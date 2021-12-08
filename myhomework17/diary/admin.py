f80f5d3ecd40ee7c7816d50eadf5794a6eae67e9
Yerin Park <1472moment@gmail.com>
2021-12-08 09:44:26 +0900

    diary 모델 / 마이그레이션 생성 및 admin에 등록

--- myhomework17/diary/admin.py
+++ myhomework17/diary/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from diary.models import Post, Tag, Comment
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
--- myhomework17/diary/models.py
+++ myhomework17/diary/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.db.models import CASCADE
 
-# Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Post(TimestampedModel):
+    author_name = models.CharField(max_length=20)
+    title = models.CharField(max_length=200, db_index=True)
+    content = models.TextField()
+    photo = models.ImageField(upload_to='diary/post/%Y/%m/%d')
+    tag_set = models.ManyToManyField('Tag', blank=True)
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=200, db_index=True)
+
+
+class Comment(TimestampedModel):
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
+    post = models.ForeignKey(Post, on_delete=CASCADE)
