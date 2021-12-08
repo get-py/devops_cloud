559a7401a592fa2f9eaddebafde42ff446bd5b71
Yerin Park <1472moment@gmail.com>
2021-12-08 10:29:59 +0900

    모델 보강 및 urls.py를 diary와 연결

--- myhomework17/diary/models.py
+++ myhomework17/diary/models.py
@@ -17,12 +17,30 @@ class Post(TimestampedModel):
     photo = models.ImageField(upload_to='diary/post/%Y/%m/%d')
     tag_set = models.ManyToManyField('Tag', blank=True)
 
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = '포스트'
+        verbose_name_plural = '포스팅 목록'
+
 
 class Tag(TimestampedModel):
     name = models.CharField(max_length=200, db_index=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = '태그'
+        verbose_name_plural = "태그 목록"
 
 
 class Comment(TimestampedModel):
     author_name = models.CharField(max_length=20)
     message = models.TextField()
     post = models.ForeignKey(Post, on_delete=CASCADE)
+
+    class Meta:
+        verbose_name = '댓글'
+        verbose_name_plural = '댓글 목록'
--- myhomework17/myhomework17/urls.py
+++ myhomework17/myhomework17/urls.py
@@ -5,6 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('diary/', include('diary.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
@@ -12,5 +13,5 @@ urlpatterns += static(settings.MEDIA_URL,
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls)),
-    ]
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls))]
