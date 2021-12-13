b1b0746fb8df25e53e6299a18319da03c36556ee
Yerin Park <1472moment@gmail.com>
2021-12-13 16:13:03 +0900

    shop_new 뷰에 구현된 Tag 저장 로직을 ShopForm 클래스로 이동

--- mydjango20/shop/views.py
+++ mydjango20/shop/views.py
@@ -39,15 +39,7 @@ def shop_new(request: HttpRequest) -> HttpResponse:
         if form.is_valid():
             saved_post = form.save()
 
-            tag_list = []
-            tags = form.cleaned_data.get("tags", "")
-            for word in tags.split(","):
-                tag_name = word.strip()
-                tag, __ = Tag.objects.get_or_create(name=tag_name)
-                tag_list.append(tag)
-            saved_post.tag_set.clear() # 간단구현을 위해 clear 호출
-            saved_post.tag_set.add(*tag_list)
-            return redirect("shop:shop_detail", saved_post.pk)
+        return redirect("shop:shop_detail", saved_post.pk)
     else:
         form = ShopForm()
     return render(request, 'shop/shop_form.html', {
