af7d417e6bcb894182ee084638cda4714dcdfbc8
Yerin Park <1472moment@gmail.com>
2021-12-15 10:33:05 +0900

    django-bootstrap-v5 설정

--- mydjango23/mydjango23/settings.py
+++ mydjango23/mydjango23/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third
+    'bootstrap5',
+    # local
+
 ]
 
 if DEBUG:
