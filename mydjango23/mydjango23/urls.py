2f5e632aaece78f1e3c3d46aa37307d4e2d30a78
Yerin Park <1472moment@gmail.com>
2021-12-15 16:19:15 +0900

    accounts 앱 생성 및 등록

--- mydjango23/mydjango23/urls.py
+++ mydjango23/mydjango23/urls.py
@@ -7,8 +7,10 @@ from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls')),
     path("blog/", include('blog.urls')),
     path('', RedirectView.as_view(url='/blog/'), name="root"),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL,
