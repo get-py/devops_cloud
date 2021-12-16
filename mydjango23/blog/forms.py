bbd5ddd7f932a54aa2a0599a48a803adcb2ab8f7
Yerin Park <1472moment@gmail.com>
2021-12-16 14:40:39 +0900

    PostForm 의 tags 를 _saved_m2m을 통해 저장

--- mydjango23/blog/forms.py
+++ mydjango23/blog/forms.py
@@ -1,9 +1,39 @@
 from django import forms
-from blog.models import Post
+from blog.models import Post, Tag
 
 
 class PostForm(forms.ModelForm):
+    tags = forms.CharField()
+
+    # 초기값 지정
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance.pk:
+            tag_qs = self.instance.tag_set.all()
+            initial = ", ".join([tag.name for tag in tag_qs])
+            self.fields["tags"].initial = initial
+
+    # DB로의 저장
+    # 아래 함수가 호출되기 전에, instance.save()가 호출되었음을 보장받습니다.
+    def _save_m2m(self):
+        super()._save_m2m()
+
+        tag_list = []
+        tags = self.cleaned_data.get("tags", '')
+        for word in tags.split(','):
+            tag_name = word.strip()
+            tag, __ = Tag.objects.get_or_create()
+            tag_list.append(tag)
+
+        self.instance.tag_set.clear()
+        self.instance.tag_set.add(*tag_list)
+
     class Meta:
         model = Post
-        fields = "__all__"
-
+        fields = [
+            "title",
+            'category',
+            'content',
+            'photo',
+        ]
