b64c8158fd575efcd30bbb088147964210d1a025
Yerin Park <1472moment@gmail.com>
2021-12-17 07:38:38 +0900

    Shop, Category, Tag, Review class 구현 및 migration, admin에 등록

--- myhomework24/blog/models.py
+++ myhomework24/blog/models.py
@@ -1,4 +1,50 @@
 from django.db import models
+from django.db.models import CASCADE
 
 
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        abstract = True
+
+
+class Shop(TimestampedModel):
+    name = models.CharField(max_length=50, db_index=True)
+    telephone = models.CharField(max_length=15)
+    description = models.TextField(blank=True)
+    tag_set = models.ManyToManyField("Tag", blank=True)
+    category = models.ForeignKey("Category", on_delete=CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Category(TimestampedModel):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Tag(TimestampedModel):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
+
+class Review(TimestampedModel):
+    shop = models.ForeignKey(Shop, on_delete=CASCADE)
+    author_name = models.CharField(max_length=20)
+    message = models.TextField()
