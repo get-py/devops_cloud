b30fe0205ebb9a1811b8d16e9bb01e7317364d73
Yerin Park <1472moment@gmail.com>
2021-12-16 17:56:40 +0900

    media 설정 수정

--- myhomework24/myhomework24/urls.py
+++ myhomework24/myhomework24/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(include(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT))
+urlpatterns += static(settings.MEDIA_URL,
+                      document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
