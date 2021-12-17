024820600ca2fec8858bf21af2d578ff207fbdc5
Yerin Park <1472moment@gmail.com>
2021-12-17 14:48:22 +0900

    layout/shop_detail/shop_list.html 구현

--- mydjango25/shop/urls.py
+++ mydjango25/shop/urls.py
@@ -1,3 +1,12 @@
+from django.urls import path
+
+from shop import views
+
 app_name = 'shop'
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.shop_list, name="shop_list"),
+    path("<int:pk>/", views.shop_detail, name="shop_detail"),
+]
+
+
