d91c7dbeafcc635a99a78c1d04b7fd3dd1ce510c
Yerin Park <1472moment@gmail.com>
2021-12-23 16:25:09 +0900

    melon

--- myreact29/src/App.js
+++ myreact29/src/App.js
@@ -1,14 +1,16 @@
 import MelonTop100 from "./MelonTop100";
 import Counter from "./Counter";
+import MelonSearch from "MelonSearch";
 
 function App() {
   return (
     <div>
       <h1> ♥ Merry Christmas ♥</h1>
-      <MelonTop100 />
+      <MelonSearch />
+      {/* <MelonTop100 />
       <Counter />
       <Counter />
-      <Counter />
+      <Counter /> */}
     </div>
   );
 }
