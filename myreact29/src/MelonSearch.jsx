f551df4e7bbb242729ecc2ab09e6e406bf88cb43
Yerin Park <1472moment@gmail.com>
2021-12-23 17:13:19 +0900

    melon 검색창 구현하기

--- myreact29/src/MelonSearch.jsx
+++ myreact29/src/MelonSearch.jsx
@@ -1,8 +1,12 @@
 import { Input } from 'antd';
 import { useState } from 'react';
+import Axios from 'axios';
+import jsonAdapter from 'axios-jsonp';
+
 
 function MelonSearch() {
     const [query, setQuery] = useState("");
+    const [songList, setSongList] = useState([]);
 
     const handleChange = (e) => {
         const { target: {value} } = e;
@@ -15,8 +19,33 @@ function MelonSearch() {
         console.group("handlePressEnter");
         console.log(`검색어 ${query}로 검색합니다.`);
         console.groupEnd();
-    };
-        
+        const url = 'https://www.melon.com/search/keyword/index.json';
+
+
+        Axios({
+            url: url,
+            adapter: jsonAdapter,
+            callbackParamName: 'jscallback', // optional, 'callback' by default
+            params: {
+                query: query,
+            }
+        }).then((response) => {
+            const { data: {SONGCONTENTS: serchedSongList }, 
+            } = response;
+            
+
+            console.group("멜론검색결과");
+            console.log(serchedSongList);
+            console.groupEnd();
+
+            setSongList(serchedSongList || []);
+        })
+        .catch((error) => {
+            console.group('멜론 검색 에러');
+            console.error(error);
+            console.groupEnd();
+        });
+    }    
 
     return <div style={{width:300, margin: '0 auto'}}>
                 <h2>멜론 검색</h2>
@@ -25,6 +54,14 @@ function MelonSearch() {
                 onChange={handleChange}
                 onPressEnter={handlePressEnter}
             />
+            {songList.map((song) => {
+                return (
+                    <div key={song.SONGID}>
+                        <img src={song.ALBUMIMG} />
+                        {song.SONGNAME} by {song.ARTISTNAME}
+                    </div>
+                );
+            })}
             </div>;
 }
 
