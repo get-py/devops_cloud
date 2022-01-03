420f36b77e6ee2432ae8fc3093ca55f0e529f0ef
Yerin Park <1472moment@gmail.com>
2022-01-03 17:12:50 +0900

    3. 하나의 상탯값에 numbers, colors 2개의 값을 담기

--- myhomework35/src/SevenNumbers.jsx
+++ myhomework35/src/SevenNumbers.jsx
@@ -1,4 +1,19 @@
+import { useState } from 'react';
+
 function SevenNumbers() {
+  const [state, setState] = useState({
+    numbers: [0, 0, 0, 0, 0, 0, 0],
+    colors: [
+      '#1B62BF',
+      '#1755A6',
+      '#37A647',
+      '#F29F05',
+      '#F23838',
+      'purple',
+      'pink',
+    ],
+  });
+
   return (
     <div>
       <h2>7개의 숫자</h2>
